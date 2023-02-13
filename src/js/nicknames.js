3203e395a5f4269f4f9c443f37afca4e5e9092ed
Nadya <nadbelyh@yandex.ru>
2023-02-13 11:28:20 +0500

    исправления

--- src/js/nicknames.js
+++ src/js/nicknames.js
@@ -1,13 +1,30 @@
+/* eslint-disable operator-linebreak */
+// export default class Validator {
+//   constructor(nickname) {
+//     this.nickname = nickname;
+//   }
+
+//   validateUsername() {
+//     return (
+//       // eslint-disable-next-line operator-linebreak
+//       /^[^\d-_][\w-]+[^\d-_]$/.test(this.nickname) &&
+//       !/\d{4,}/.test(this.nickname)
+//     );
+//   }
+// }
+
 export default class Validator {
   constructor(nickname) {
     this.nickname = nickname;
   }
 
   validateUsername() {
-    return (
-      // eslint-disable-next-line operator-linebreak
-      /^[^\d-_][\w-]+[^\d-_]$/.test(this.nickname) &&
-      !/\d{4,}/.test(this.nickname)
-    );
+    const onlyLettersAndNumbersAndSymbols = /^[\w-]*$/.test(this.nickname); // Допустимы только латинские буквы , символы тире -, подчёркивания _ и цифры (0-9);
+    const exeptThreeNumbersInRow = !/(\d{4,})+/g.test(this.nickname); // Имя не должно содержать подряд более трёх цифр.
+    const edgeCheck = /^[^\d-_].*[^\d-_]$/.test(this.nickname); // На краях не должно быть цифр, подчеркивания и тире
+    const result =
+      onlyLettersAndNumbersAndSymbols && exeptThreeNumbersInRow && edgeCheck;
+
+    return result;
   }
 }
